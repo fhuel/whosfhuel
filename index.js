e41ca1420178751019f095bb8acccd7597d8db7c
manuele.capacci@gmail.com <manuele.capacci@gmail.com>
2020-11-30 11:56:20 +0100

    first version

---
+++ index.js
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+console.log(`
+Manuele Capacci
+
+Designer and developer using machines to create beautiful and meaningful products, visuals, and experiences for humans.
+
+Founder and director at OurFir.es, London based design studio.
+
+You can find me on:
+🌍 Website - https://manuelecapacci.com
+🐦 Twitter - https://twitter.com/ManueleCapacci
+📖 GitHub - https://github.com/fhuel
+💼 Linkedin - https://www.linkedin.com/in/manuelecapacci/
+🌁 Instagram - https://www.instagram.com/fhuel/
+
+`);
